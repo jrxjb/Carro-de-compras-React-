2bf53ed2da92adcdc4a781e810942ec7cbbce0df
JB <177959714+jrxjb@users.noreply.github.com>
2026-06-18 00:06:21 -0400

    add and less element, add fiter by price and category

--- carro-de-compras/src/app.jsx
+++ carro-de-compras/src/app.jsx
@@ -2,9 +2,66 @@ import GetUrl from "./products/products"
 import { useEffect, useState } from "react";
 export default function App(){
 
-   return(<>
-<p>Prueba</p>
-<GetUrl/>
-    </>)
+const [seachByCa,SetSeachByCa]=useState("all")
+const [category,serCategory] =useState("")
+const [priceCategory,SetPriceCategory] = useState("all")
+const [addToCar,SetAddToCar] = useState("none")
+const [renderAddToCar,setRenderAddToCar] = useState([])
+
+function categorySeach(){
+SetSeachByCa(!seachByCa)
 }
 
+
+//añadir al carrito 
+
+function handleAddToCar(producto){
+setRenderAddToCar([...renderAddToCar,producto])
+}
+
+
+function handleRemovetoCar(id){
+const NewToRemove = renderAddToCar.filter ( (item)=> item.id !==id)
+
+setRenderAddToCar(NewToRemove)
+}
+
+	 return(
+<>
+		<header>
+<section>
+		<button onClick={()=>{SetSeachByCa("electronics")}}>Electronicos</button>
+		<button onClick={()=>{SetSeachByCa("women's clothing")}}>women's</button>
+		<button onClick={()=>{SetSeachByCa("jewelery")}}>jewelery</button>
+		<button onClick={()=>{SetSeachByCa("men's clothing")}}>Men's</button>
+		<button onClick={()=>{SetSeachByCa("all")}}>All</button>
+</section>
+<section>
+		<button onClick={()=>{SetPriceCategory(10) }}>10 or less</button>
+		<button onClick={()=>{SetPriceCategory(50) }}>50 or less</button>
+		<button onClick={()=>{SetPriceCategory(100) }}>100 or less</button>
+		<button onClick={()=>{SetPriceCategory(200) }}>200 or less</button>
+		<button onClick={()=>{SetPriceCategory(500) }}>500 or less</button>
+		<button onClick={()=>{SetPriceCategory(1000) }}>1000 or less</button>
+		<button onClick={()=>{SetPriceCategory("all") }}>All price</button>
+</section>
+		</header>
+		<section>
+				<section>
+					<h2>Carrito</h2>
+						{renderAddToCar.map((item,index) => {
+							return(
+								 <section key={index}>
+									{item.title}
+									<img src={item.image} alt="" />
+									<button onClick={()=>handleRemovetoCar(item.id)} >Eliminar</button>
+							</section>)
+						})}
+				</section>
+				<GetUrl filter={seachByCa} price={priceCategory} alHacerClickToAdd={handleAddToCar} >
+				
+				</GetUrl>
+		</section>
+
+</>)
+}
--- carro-de-compras/src/products/products.jsx
+++ carro-de-compras/src/products/products.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
 
-export default function GetUrl(){
+export default function GetUrl({filter,price,children,alHacerClickToAdd}){
 const [data, setData] = useState([])
+
 
 useEffect(()=>{
     async function getData(){
@@ -11,12 +12,44 @@ useEffect(()=>{
     }
     getData()
 },[])
+
+const dataFiltrada= filter=="all"? data: data.filter((item)=>item.category ===filter) 
+
+//Filtrar por precio 
+
+const dataPrice = price =="all"? dataFiltrada : dataFiltrada.filter((item)=>item.price <= price)
+
+
 return(
 <div> 
-    {data.map((producto)=>{
+    {dataPrice.map((producto)=>{
         return(
-            <p key={producto.id}>{producto.title}</p>
+            <section key={producto.id}>
+                <p> Categoria {producto.category}</p>
+                <p>Titulo {producto.title}</p>
+                <p>Precio {producto.price}$</p>
+                <p>Descripcion {producto.description}</p>
+                <p>Rating {producto.rating.rate}</p>
+                <p> Count del producto {producto.rating.count}</p>
+                <img src={producto.image} alt={`imagen from ${producto.title}`} />
+                <button onClick={()=>alHacerClickToAdd(producto)}> <h1>Añadir al carrito </h1></button>
+               
+
+            </section>
         )
     })} 
 </div>) 
 }
+
+
+/*
+    "id
+    "title
+    "price
+    "description 
+    "category
+    "image": "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_t.png",
+    "rating
+      "rate
+      count
+*/
